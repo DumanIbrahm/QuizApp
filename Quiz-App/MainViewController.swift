d1ac948c70aa0d36e3ccca4e8a50a3b8c0b1da8b
Ibrahim Duman <dumanibrahm@gmail.com>
2023-02-10 21:19:09 +0300

    Gerekli bağlantılar yapıldı

--- Quiz-App/MainViewController.swift
+++ Quiz-App/MainViewController.swift
@@ -38,6 +38,7 @@ class MainViewController: UIViewController {
     @IBOutlet weak var BtnC: UIButton!
     @IBOutlet weak var BtnB: UIButton!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         timeCount()
